25d43f7d67d67aa79f33366d0c4c01f457941a98
Christopher <yanezchristopher55@gmail.com>
2019-12-21 14:18:34 -0800

    Added feature to comment on blog posts.

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # The bottom import is importing the class Post from models.py
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
